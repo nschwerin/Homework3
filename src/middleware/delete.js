7649a7867293dfb9460c810380cb60c39125afb5
Nicole <nschwerin@ltu.edu>
2021-04-05 10:53:16 -0400

    Update delete.js

---
+++ src/middleware/delete.js
@@ -0,0 +1,10 @@
+const { StatusCodes } = require('http-status-codes');
+
+exports.delete = (err, req, res, next) => {
+  if( req.method === 'DELETE') {
+    res.status(StatusCodes.METHOD_NOT_ALLOWED).send(`We're sorry, the error was: ${err.message}`);
+  }
+  else {
+    next();
+  }
+ };
