e143e445c26f4bf6af0fc5492ae7cb44d20e22e2
Nicole <nschwerin@ltu.edu>
2021-04-05 01:32:06 -0400

    work on dateValidation

--- src/middleware/dateValidation.js
+++ src/middleware/dateValidation.js
@@ -1,7 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 // eslint-disable-next-line no-unused-vars
-const getdate = (search) => {
+const getDate = (search) => {
   let result = null;
   Object.key(search).forEach((key) => {
     if (key.toLowerCase() === 'date-validation') {
@@ -10,3 +10,28 @@ const getdate = (search) => {
   });
   return result;
 };
+
+const ten =10;
+const min5 = 300;
+const thousand = 1000;
+//for calculating time using functions
+
+exports.dateValidator = ((req, res, next) => {
+
+  const queryDate = getDate()(req.query);
+  const headerDate = getDate(req.header); //header and query date stored, math next to get number
+
+  const currentDate = Math.round(Date.now() / thousand);
+  if ((queryDate != null) && !(Number.isNaN(queryDate))) {
+    const reqDate = Number.parseInt(queryDate, ten);
+  }
+  else if (headerDate != null && !(Number.isNaN(headerDate))){
+    const reqDate = Number.parseInt(headerDate, ten);
+  }
+  else { return false}
+
+
+
+
+
+});
--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
 
-module.exports = (err, req, res, next) => {
+exports = (err, req, res, next) => {
   res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Error message');
 };
