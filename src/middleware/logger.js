0b5bfe9f0261879d56f900e7350fbe2724816bd1
Nicole <nschwerin@ltu.edu>
2021-04-05 12:35:06 -0400

    create logger

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,1 +1,23 @@
 const winston = require('winston');
+
+const loggerW = winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  ]
+})
+
+exports.logger = (req, res, next) => {
+  loggerW.log({
+    level: 'info',
+    serverTime: req.currentDate,
+    method: req.method,
+    url: req.originalUrl,
+    body: req.body,
+    query: req.query,
+    headers: req.headers,
+    dateValidation: req.dateValidation,
+  });
+  next();
+};
