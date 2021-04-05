2e32e3571c3d8a3ff870b32be9e5e6d467d79b64
Nicole <nschwerin@ltu.edu>
2021-04-05 01:53:27 -0400

    Update dateValidation.js

--- src/middleware/dateValidation.js
+++ src/middleware/dateValidation.js
@@ -3,6 +3,7 @@ const { StatusCodes } = require('http-status-codes');
 // eslint-disable-next-line no-unused-vars
 const getDate = (search) => {
   let result = null;
+  //get date
   Object.key(search).forEach((key) => {
     if (key.toLowerCase() === 'date-validation') {
       result = search[key];
@@ -21,14 +22,23 @@ exports.dateValidator = ((req, res, next) => {
   const queryDate = getDate()(req.query);
   const headerDate = getDate(req.header); //header and query date stored, math next to get number
 
-  const currentDate = Math.round(Date.now() / thousand);
-  if ((queryDate != null) && !(Number.isNaN(queryDate))) {
-    const reqDate = Number.parseInt(queryDate, ten);
+  let reqDate;
+  if (((queryDate || headerDate) != null) && !(Number.isNaN(queryDate) && !(Number.isNaN(headerDate)))) {
+
+    const currentDate = Math.round(Date.now() / thousand);
+    if (queryDate != null) {
+      reqDate = Number.parseInt(queryDate, ten);
+    } else {
+      reqDate = Number.parseInt(headerDate, ten);
+    }
+    if ((min5 > currentDate - reqDate) && (min5 > reqDate - currentDate)) {
+      
+    }
+  } else {
+    res.sendStatus(StatusCodes.METHOD_NOT_ALLOWED);
   }
-  else if (headerDate != null && !(Number.isNaN(headerDate))){
-    const reqDate = Number.parseInt(headerDate, ten);
-  }
-  else { return false}
+
+
 
 
 
