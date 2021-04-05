8915a84f3d28ed71503ad5a3aaa3f9b220dbbaab
Nicole <nschwerin@ltu.edu>
2021-04-05 14:16:34 -0400

    add routes

--- src/middleware/deleteR.js
+++ src/middleware/deleteR.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require('http-status-codes');
 
-exports.delete = (err, req, res, next) => {
+exports.deleteR = (err, req, res, next) => {
   if (req.method === 'DELETE') {
     res.status(StatusCodes.METHOD_NOT_ALLOWED).send(`We're sorry, the error was: ${err.message}`);
   } else {
--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 const Express = require('express');
 const BodyParser = require('body-parser');
 
+const dateValidation = require('./middleware/dateValidation');
+const deleteR = require('./middleware/deleteR');
+const errors = require('./middleware/errors');
+const logger = require('./middleware/logger');
+
 const app = Express();
 
 app.use(BodyParser.json());
+app.use(deleteR.deleteR);
+app.use(dateValidation.dateValidation);
+app.use(logger.logger);
+app.use(errors.errors);
 
 app.listen(8080);
