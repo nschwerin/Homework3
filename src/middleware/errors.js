7abe509019f48b45ac51b5bffa7578ad7d5129a1
Nicole <nschwerin@ltu.edu>
2021-04-05 02:16:23 -0400

    correct errors message

--- src/middleware/errors.js
+++ src/middleware/errors.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
 
-exports = (err, req, res, next) => {
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Error message');
+exports.errors = (err, req, res, next) => {
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(`We're sorry, the error was: ${err.message}`);
 };
